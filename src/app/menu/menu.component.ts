dd9db296e26765e0605af7db2b385f26ffb203d9
Nicolas Janel <janel.nicolas@zenside.fr>
2018-10-23 16:23:50 +0200

    Data binding

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../models/user.model';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  menuItems:string[] = [
+    "Taches",
+    "Idées",
+    "Mon compte",
+    "Mes taches",
+    "Mes idées"
+  ]
+
+  currentUser: User;
+
   constructor() { }
 
   ngOnInit() {
+    this.currentUser = new User();
+    this.currentUser.name = "Roger Dupont";
+    this.currentUser.email = "xxx@xxx.xxx";
   }
 
 }
